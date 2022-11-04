2f75abebf05a705de6a5343f6172b0e905aeb0dc
Demi Hayashi <demi.h@me.com>
2022-11-04 16:59:41 -0700

    update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,12 +2,14 @@ import * as React from "react";
 
 // import components
 import Header from '../components/header';
+import Home from '../components/home';
 import Footer from "../components/footer";
 
 const IndexPage = () => {
   return (
     <main>
       <Header />
+      <Home />
       <Footer />
     </main>
   );
