5841192ab25847c3a9809bcef089560225f4cd59
Andrea Jared <adj0803@gmail.com>
2022-11-05 14:13:51 -0400

    working on authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,8 +20,24 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-            return user;
+            const token = signToken(user);
+            return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findone({ email });
+
+      if(!user) {
+        throw new AuthenticationError('Your email or password dont match!');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+    if (!correctPw) {
+      throw new AuthenticationError('Please enter correct email or password');
     }
+
+    const token = signToken(user);
+    return { token, user };
+    },
+    
   }
 }
   
