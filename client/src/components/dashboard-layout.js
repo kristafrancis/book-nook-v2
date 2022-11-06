c78024403695495fae3050aadc8935e5b6e9091b
Demi Hayashi <demi.h@me.com>
2022-11-05 19:48:44 -0700

    initial cards set up in dashboard.js

--- client/src/components/dashboard-layout.js
+++ client/src/components/dashboard-layout.js
@@ -13,23 +13,27 @@ const Dashboard = ({ pageTitle, children }) => {
                 >
                 </div>
             </header>
+
             <nav>
-                <div className="w-[80%] flex justify-between items-center mx-auto">
-                    <h1 className="font-light drop-shadow-lg py-4 text-5xl mt-4 md:text-7xl md:mt-10">
+                <div className="w-[80%] my-8 flex justify-between items-center mx-auto">
+                    <h1 className="font-light drop-shadow-lg py-4 text-5xl md:text-7xl">
                         <Link to="/dashboard" className="text-[#027bce] hover:text-[#03cea4]">Book<span className="text-[#03cea4]">Nook</span></Link>
                     </h1>
                     <ul className="h-full flex items-center list-none z-[5] text-base">
-                        <li className="text-[#027bce] italic mr-4">Travel to the stars, read!</li>
+                        {/* NAVIGATION LINKS HERE */}
+                        <li className="mr-4">Link Here</li>
                         <li className="mx-4">Link Here</li>
                         <li className="mx-4">Link Here</li>
                         <li className="ml-4">Link Here</li>
                     </ul>
                 </div>
             </nav>
-            <main className="w-full md:h-[94vh]">
+
+            <main className="z-[5] w-full md:h-screen">
                 {children}
             </main>
-            <footer class="z-[5] -mt-[38%] text-center mix-blend-color-dodge md:mix-blend-multiply md:mt-0 md:p-3">
+
+            <footer class="z-[5] -mt-[38%] text-center mix-blend-color-dodge md:mix-blend-multiply md:mt-0 md:p-4">
                 <p class="md:text-base text-sm tracking-wider font-medium text-gray-400">
                     &copy; {new Date().getFullYear()} The Spice Girls, LLC.
                 </p>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import HomeLayout from '../components/homelayout';
+import HomeLayout from '../components/home-layout';
 import Auth from '../utils/auth';
 
 const Signup = () => {
   return (
     <HomeLayout pageTitle="Sign Up">
+      {/* SIGNUP FORM HERE & ADD LINK TO LOGIN PAGE */}
       <p>Signup Test</p>
     </HomeLayout>
   )
