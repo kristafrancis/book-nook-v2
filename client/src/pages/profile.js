e793b965d97c59b68210bdb16d7cb8b09a95c657
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-11-08 18:33:01 -0500

    edit profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -84,12 +84,14 @@ const Profile = () => {
               >
                 ADD TO READING LIST
               </button>
-              <button
-                type="button"
-                class="inline-flex items-center justify-center rounded-md border border-transparent bg-[#027bce] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-              >
-                EDIT GOAL
-              </button>
+              <a href="/editprofile">
+                <button
+                  type="button"
+                  class="inline-flex items-center justify-center rounded-md border border-transparent bg-[#027bce] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                >
+                  EDIT PROFILE
+                </button>{" "}
+              </a>
             </div>
           </div>
 
