e71b3c304b9c68bcc1ce29a28c4aa9c79bb63334
Demi Hayashi <demi.h@me.com>
2022-11-09 17:48:43 -0800

    fix bg issue in profile.js; set dashboard layout in editprofile.js

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,53 +1,12 @@
 import React from "react";
-import { Link } from "gatsby";
 import DashboardLayout from "../components/dashboard-layout";
-import Footer from "../components/footer";
 
 const Profile = () => {
   return (
-    <div>
-      <title>Book Nook | Profile</title>
-      <header>
-        {/* <div
-          className="stars-bg bg-fixed bg-repeat-x bg-top absolute top-0 left-0 w-full h-full z-0"
-          style={{ backgroundImage: `url(${bgImg})` }}
-        ></div> */}
-      </header>
+    <DashboardLayout pageTitle="Profile">
 
-      <nav>
-        <div className="w-[80%] my-8 flex justify-between items-center mx-auto">
-          <h1 className="font-light drop-shadow-lg py-4 text-5xl md:text-7xl">
-            <Link to="/search" className="text-[#027bce] hover:text-[#03cea4]">
-              Book<span className="text-[#03cea4]">Nook</span>
-            </Link>
-          </h1>
-          <ul className="h-full flex items-center list-none z-[5] text-base">
-            {/* NAVIGATION LINKS HERE */}
-            <a
-              class="bg-gray-100 p-2 rounded-lg hover:text-sky-900 font-semibold mr-6"
-              href="/search"
-            >
-              Search
-            </a>
-
-            <a
-              class="bg-gray-100 p-2 rounded-lg hover:text-sky-900  font-semibold mr-6"
-              href="/profile"
-            >
-              Profile
-            </a>
-            <a
-              class="bg-gray-100 p-2 rounded-lg hover:text-sky-900  font-semibold mr-6"
-              href="#"
-            >
-              Logout
-            </a>
-          </ul>
-        </div>
-      </nav>
-
-      <div class="min-h-full bg-sky-50">
-        <main class="py-10">
+      <div class="min-h-full">
+        <main class="pb-10">
           <div class="mx-auto max-w-3xl px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
             <div class="flex items-center space-x-5">
               <div class="flex-shrink-0">
@@ -342,8 +301,7 @@ const Profile = () => {
         </main>
       </div>
 
-      <Footer />
-    </div>
+    </DashboardLayout>
   );
 };
 export default Profile;
