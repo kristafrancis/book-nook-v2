6c88962ecef49d638068761fa3078fbeeddab531
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-11-11 22:48:10 -0500

    NO GATSBY

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project-3',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/develop',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
