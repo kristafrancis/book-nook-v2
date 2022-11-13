c412c5767263af8baf19ea29a29eafd3e65a88bf
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-11-13 14:29:06 -0500

    trending

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -26,12 +26,12 @@ const Search = () => {
       date: 'Mar 16, 2020',
       datetime: '2020-03-16',
       imageUrl:
-        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       author: {
         name: 'Roel Aufderehar',
         href: '#',
         imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       },
     },
     {
@@ -42,12 +42,12 @@ const Search = () => {
       date: 'Mar 10, 2020',
       datetime: '2020-03-10',
       imageUrl:
-        'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       author: {
         name: 'Brenna Goyette',
         href: '#',
         imageUrl:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       },
     },
     {
@@ -58,12 +58,12 @@ const Search = () => {
       date: 'Feb 12, 2020',
       datetime: '2020-02-12',
       imageUrl:
-        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       author: {
         name: 'Daniela Metz',
         href: '#',
         imageUrl:
-          'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
       },
     },
   ]
@@ -207,7 +207,7 @@ const Search = () => {
                   {/* {Auth.loggedIn() && ( */}
                   <div class="pt-10">
                   <button
-                    className="float-right inline-flex items-center justify-center rounded-md border border-indigo-200 bg-[#22274f] px-4 py-2 text-sm font-medium shadow-sm"
+                    className="float-right inline-flex items-center justify-center rounded-md border border-indigo-200 bg-[#22274f] px-4 py-2 text-sm font-medium shadow-md"
                     disabled={savedBookIds?.some(
                       (savedBookId) => savedBookId === book.bookId
                     )}
@@ -233,7 +233,7 @@ const Search = () => {
 
 
         <div id="Trending"></div>
-        <div className="relative bg-transparent px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+        <div className="relative bg-transparent px-4 pt-16 sm:px-6 lg:px-8 lg:pt-24 pb-10">
       <div className="absolute inset-0">
         <div className="h-1/3 bg-transparent sm:h-2/3" />
       </div>
@@ -247,12 +247,12 @@ const Search = () => {
         <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <div key={post.title} className="flex flex-col overflow-hidden rounded-lg shadow-lg">
-              <div className="flex-shrink-0">
-                <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
-              </div>
+             
               <div className="flex flex-1 flex-col justify-between bg-slate-900 p-6">
                 <div className="flex-1">
-
+                <div className="flex-shrink-0">
+                <img className="float-left pr-5" src={post.imageUrl} alt="" />
+              </div>
                   <a href={post.href} className="mt-2 block">
                     <p className="text-xl font-semibold">{post.title}</p>
                     <p className="mt-3 text-base text-white">{post.description}</p>
