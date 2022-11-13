8237f39daf51c50c717b95d5ef6dcfe1e956fc5f
Andrea Jared <adj0803@gmail.com>
2022-11-13 16:16:13 -0500

    recommit

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Burger from '../Burger/Burger';
 import Auth from '../../utils/auth';
-
+//recommit
 const Navbar = () => {
     const logout = event => {
         event.preventDefault();
