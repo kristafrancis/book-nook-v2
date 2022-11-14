6db2a16e9b3da133d4287a447707fe31b3aa2f4d
Demi Hayashi <demi.h@me.com>
2022-11-14 00:02:03 -0800

    update css in Editprofile.js

--- client/src/pages/Editprofile.js
+++ client/src/pages/Editprofile.js
@@ -7,16 +7,14 @@ const EditProfile = () => {
   return (
     <>
       <Layout pageTitle="Edit Profile">
-        <main className="flex-1 bg-slate-900">
-          <div className="relative mx-auto max-w-4xl md:px-8 xl:px-0">
-            <div className="pt-10 pb-16">
-              <div className="px-4 sm:px-6 md:px-0">
-                <h1 className="text-3xl font-bold tracking-tight">
-                  Settings
-                </h1>
-              </div>
-              <div className="px-4 sm:px-6 md:px-0">
-                <div className="py-6">
+        <main className="max-w-screen-md h-full mx-auto">
+
+          <h1 className="my-8 ml-4 text-4xl font-semibold drop-shadow">
+            Settings
+          </h1>
+
+          <div className="flex justify-center items-center flex-1 bg-slate-900 rounded-lg shadow-lg">
+              <div className="w-[90%] py-3 px-4 sm:px- md:px-0">
                   <div className="lg:hidden">
                     <label for="selected-tab" className="sr-only">
                       Select a tab
@@ -24,7 +22,7 @@ const EditProfile = () => {
                     <select
                       id="selected-tab"
                       name="selected-tab"
-                      className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
+                      className="mt-1 block w-full rounded-md bg-[#22274f] border-indigo-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     >
                       <option selected>General</option>
 
@@ -32,18 +30,18 @@ const EditProfile = () => {
                     </select>
                   </div>
                   <div className="hidden lg:block">
-                    <div className="border-b border-gray-200">
+                    <div className="border-b divide-slate-700">
                       <nav className="-mb-px flex space-x-8">
                         <a
                           href="#"
-                          className="border-teal-500  whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                          className="border-indigo-500  whitespace-nowrap py-4 px-1 border-b-2 font-medium text-normal"
                         >
                           General
                         </a>
 
                         <a
                           href="#"
-                          className="border-transparent whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                          className="border-transparent whitespace-nowrap py-4 px-1 border-b-2 font-medium text-normal"
                         >
                           Password
                         </a>
@@ -51,28 +49,28 @@ const EditProfile = () => {
                     </div>
                   </div>
 
-                  <div className="mt-10 divide-y divide-gray-200">
+                  <div className="mt-10 divide-y divide-slate-700">
                     <div className="space-y-1">
-                      <h3 className="text-lg font-medium leading-6">
+                      <h3 className="text-2xl font-medium">
                         Profile
                       </h3>
-                      <p className="max-w-2xl text-sm">
+                      <p className="max-w-2xl">
                         This information will be displayed publicly so be
                         careful what you share.
                       </p>
                     </div>
                     <div className="mt-6">
-                      <dl className="divide-y divide-gray-200">
+                      <dl className="divide-y divide-slate-700">
                         <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
-                          <dt className="text-sm font-medium">
+                          <dt className="font-medium text-indigo-400">
                             Name
                           </dt>
-                          <dd className="mt-1 flex text-sm sm:col-span-2 sm:mt-0">
+                          <dd className="mt-1 flex sm:col-span-2 sm:mt-0">
                             <span className="flex-grow">Bob Joe</span>
                             <span className="ml-4 flex-shrink-0">
                               <button
                                 type="button"
-                                className="rounded-md font-medium hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-900  focus:ring-offset-2"
+                                className="rounded font-medium hover:text-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
                               >
                                 Update
                               </button>
@@ -80,10 +78,10 @@ const EditProfile = () => {
                           </dd>
                         </div>
                         <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:pt-5">
-                          <dt className="text-sm font-medium">
+                          <dt className="flex items-center font-medium text-indigo-300">
                             Photo
                           </dt>
-                          <dd className="mt-1 flex text-sm sm:col-span-2 sm:mt-0">
+                          <dd className="mt-1 flex sm:col-span-2 sm:mt-0">
                             <span className="flex-grow">
                               <img
                                 className="h-16 w-16 rounded-full"
@@ -91,7 +89,7 @@ const EditProfile = () => {
                                 alt=""
                               ></img>{" "}
                             </span>
-                            <span className="ml-4 flex flex-shrink-0 items-start space-x-4">
+                            <span className="ml-4 flex flex-shrink-0 items-center space-x-4">
                               <button
                                 type="file"
                                 name="myImage"
@@ -99,7 +97,7 @@ const EditProfile = () => {
                                   console.log(event.target.files[0]);
                                   setSelectedImage(event.target.files[0]);
                                 }}
-                                className="rounded-md font-medium hover:text-text-blue-700 bg-slate-900 focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-offset-2"
+                                className="rounded-md font-medium hover:text-indigo-200 bg-slate-900 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
                               >
                                 Update
                               </button>
@@ -108,7 +106,7 @@ const EditProfile = () => {
                               </span>
                               <button
                                 type="button"
-                                className="rounded-md font-medium hover:text-text-blue-700  focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-offset-2"
+                                className="rounded font-medium hover:text-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
                               >
                                 Remove
                               </button>
@@ -116,17 +114,17 @@ const EditProfile = () => {
                           </dd>
                         </div>
                         <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:pt-5">
-                          <dt className="text-sm font-medium">
+                          <dt className="font-medium text-indigo-300">
                             Email
                           </dt>
-                          <dd className="mt-1 flex text-sm sm:col-span-2 sm:mt-0">
+                          <dd className="mt-1 flex sm:col-span-2 sm:mt-0">
                             <span className="flex-grow">
                               bob.joe@spicegirls.com
                             </span>
                             <span className="ml-4 flex-shrink-0">
                               <button
                                 type="button"
-                                className="rounded-md font-medium hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-offset-2"
+                                className="rounded font-medium hover:text-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
                               >
                                 Update
                               </button>
@@ -138,10 +136,8 @@ const EditProfile = () => {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+              
         </main>
-
       </Layout>
     </>
   );
