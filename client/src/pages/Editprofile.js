222db74b8e5f987a45d8217b9342b59e2a4a1fc6
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-11-14 18:11:13 -0500

    signup/login icons

--- client/src/pages/Editprofile.js
+++ client/src/pages/Editprofile.js
@@ -35,40 +35,6 @@ const EditProfile = () => {
 
         <div className="flex justify-center items-center flex-1 bg-slate-900 rounded-lg shadow-lg">
           <div className="w-[90%] py-3 px-4 sm:px- md:px-0">
-            <div className="lg:hidden">
-              <label for="selected-tab" className="sr-only">
-                Select a tab
-              </label>
-              <select
-                id="selected-tab"
-                name="selected-tab"
-                className="mt-1 block w-full rounded-md bg-[#22274f] border-indigo-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-              >
-                <option selected>General</option>
-
-                <option>Password</option>
-              </select>
-            </div>
-            <div className="hidden lg:block">
-              <div className="border-b divide-slate-700">
-                <nav className="-mb-px flex space-x-8">
-                  <a
-                    href="#"
-                    className="border-indigo-500  whitespace-nowrap py-4 px-1 border-b-2 font-medium text-normal"
-                  >
-                    General
-                  </a>
-
-                  <a
-                    href="#"
-                    className="border-transparent whitespace-nowrap py-4 px-1 border-b-2 font-medium text-normal"
-                  >
-                    Password
-                  </a>
-                </nav>
-              </div>
-            </div>
-
             <div className="mt-10 divide-y divide-slate-700">
               <div className="space-y-1">
                 <h3 className="text-2xl font-medium">Profile</h3>
@@ -147,6 +113,20 @@ const EditProfile = () => {
                     <dt className="font-medium text-indigo-300">Email</dt>
                     <dd className="mt-1 flex sm:col-span-2 sm:mt-0">
                       <span className="flex-grow">bob.joe@spicegirls.com</span>
+                      <span className="ml-4 flex-shrink-0">
+                        <button
+                          type="button"
+                          className="rounded font-medium hover:text-indigo-200"
+                        >
+                          Update
+                        </button>
+                      </span>
+                    </dd>
+                  </div>
+                  <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:pt-5">
+                    <dt className="font-medium text-indigo-300">Password</dt>
+                    <dd className="mt-1 flex sm:col-span-2 sm:mt-0">
+                      <span className="flex-grow">**********</span>
                       <span className="ml-4 flex-shrink-0">
                         <button
                           type="button"
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,124 +5,155 @@ import Auth from "../utils/auth";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-	const [formState, setFormState] = useState({ email: "", password: "" });
-	// const [validated] = useState(false);
-	const [loginUser] = useMutation(USER_LOGIN);
-	const navigate = useNavigate();
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  // const [validated] = useState(false);
+  const [loginUser] = useMutation(USER_LOGIN);
+  const navigate = useNavigate();
 
-	const handleInputChange = (event) => {
-		const { name, value } = event.target;
-		setFormState({ ...formState, [name]: value });
-	};
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
 
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
-		navigate("/search");
-		console.log(formState);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    navigate("/search");
+    console.log(formState);
 
-		// // check if form has everything (as per react-bootstrap docs)
-		// const form = event.currentTarget;
-		// if (form.checkValidity() === false) {
-		//   event.preventDefault();
-		//   event.stopPropagation();
-		// }
+    // // check if form has everything (as per react-bootstrap docs)
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
-		try {
-			const { data } = await loginUser({
-				variables: { ...formState },
-			});
+    try {
+      const { data } = await loginUser({
+        variables: { ...formState },
+      });
 
-			//const { token, user } = await response.json();
-			// console.log(user);
-			Auth.login(data.login.token);
-		} catch (err) {
-			console.error(err);
-		}
+      //const { token, user } = await response.json();
+      // console.log(user);
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
 
-		setFormState({
-			email: "",
-			password: "",
-		});
-	};
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
 
-	//const Login = () => {
+  //const Login = () => {
 
-	return (
-		<main className="w-full h-full">
-			<h2 className="pb-8 text-5xl text-center font-medium drop-shadow">
-				Welcome Back
-			</h2>
+  return (
+    <main className="w-full h-full">
+      <h2 className="pb-8 text-5xl text-center font-medium drop-shadow">
+        Welcome Back
+      </h2>
 
-			<div className="w-[500px] mx-auto bg-slate-900 p-10 rounded-lg shadow-lg">
-				<form
-					className="w-[full]"
-					action="#"
-					method="POST"
-					onSubmit={handleFormSubmit}
-				>
-					<input type="hidden" name="remember" value="true"></input>{" "}
-					<div className="rounded-md shadow-lg">
-						<div>
-							<label for="email-address" className="sr-only">
-								Email address
-							</label>
-							<input
-								id="email-address"
-								name="email"
-								type="email"
-								autocomplete="email"
-								required
-								onChange={handleInputChange}
-								value={formState.email}
-								className="relative block w-full appearance-none rounded-none rounded-t-md bg-[#22274f] border border-slate-700 px-3 py-2 text-sm md:text-lg text-gray-200 placeholder-slate-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-								placeholder="Email address"
-							></input>{" "}
-						</div>
-						<div>
-							<label for="password" className="sr-only">
-								Password
-							</label>
-							<input
-								id="password"
-								name="password"
-								type="password"
-								autocomplete="current-password"
-								required
-								onChange={handleInputChange}
-								value={formState.password}
-								className="relative block w-full appearance-none rounded-none rounded-b-md bg-[#22274f] border border-slate-700 px-3 py-2 text-sm md:text-lg text-gray-200 placeholder-slate-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-								placeholder="Password"
-							></input>{" "}
-						</div>
-					</div>
-					<div className="flex items-center justify-between">
-						<div className="text-sm pt-4 pb-6">
-							<a href="/signup" className="font-medium">
-								Don't have an account? Sign up.
-							</a>
-						</div>
-					</div>
-					<div>
-						<button
-							disable={!(formState.email && formState.password)}
-							type="submit"
-							variant="sucess"
-							className="bg-[#7C87f2] w-full group flex justify-center rounded-md py-1 text-base md:text-lg font-medium focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>
-							<span className="absolute inset-y-0 left-0 flex items-center pl-3">
-								<path
-									fill-rule="evenodd"
-									d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-									clip-rule="evenodd"
-								/>
-							</span>
-							Sign in
-						</button>
-					</div>
-				</form>
-			</div>
-		</main>
-	);
+      <div className="w-[500px] mx-auto bg-slate-900 p-10 rounded-lg shadow-lg">
+        <form
+          className="w-[full]"
+          action="#"
+          method="POST"
+          onSubmit={handleFormSubmit}
+        >
+          <input type="hidden" name="remember" value="true"></input>{" "}
+          <div className="rounded-md shadow-lg">
+            <div class="relative rounded-md shadow-sm">
+              <label for="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                onChange={handleInputChange}
+                value={formState.email}
+                className="relative block w-full appearance-none rounded-none rounded-t-md bg-[#22274f] border border-slate-700 px-3 py-2 text-sm md:text-lg text-gray-200 placeholder-slate-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                placeholder="Email address"
+              ></input>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-auto text-slate-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div class="relative rounded-md shadow-sm">
+              <label for="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                onChange={handleInputChange}
+                value={formState.password}
+                className="relative block w-full appearance-none rounded-none rounded-b-md bg-[#22274f] border border-slate-700 px-3 py-2 text-sm md:text-lg text-gray-200 placeholder-slate-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                placeholder="Password"
+              ></input>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-auto text-slate-500"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="text-sm pt-4 pb-6">
+              <a href="/signup" className="font-medium">
+                Don't have an account? Sign up.
+              </a>
+            </div>
+          </div>
+          <div>
+            <button
+              disable={!(formState.email && formState.password)}
+              type="submit"
+              variant="sucess"
+              className="bg-[#7C87f2] w-full group flex justify-center rounded-md py-1 text-base md:text-lg font-medium focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <path
+                  fill-rule="evenodd"
+                  d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                  clip-rule="evenodd"
+                />
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
