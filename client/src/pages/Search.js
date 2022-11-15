e981bc86fbeee8779de4f45fe719d803ea12a093
Demi Hayashi <demi.h@me.com>
2022-11-14 21:06:31 -0800

    edit icons in login & signup

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -103,7 +103,7 @@ const Search = () => {
 						<div className="relative">
 							<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
 								<svg
-									className="h-5 w-5 text-gray-300"
+									className="h-5 w-5 text-teal-300"
 									xmlns="http://www.w3.org/2000/svg"
 									viewBox="0 0 20 20"
 									fill="currentColor"
@@ -121,7 +121,7 @@ const Search = () => {
 								value={searchInput}
 								onChange={(e) => setSearchInput(e.target.value)}
 								type="text"
-								className="block w-full rounded-md  bg-slate-900 border border-slate-600 text-gray-200 placeholder-slate-500 mt-2 mb-4 py-2 pl-10 pr-3 text-sm focus:border-indigo-500 focus:text-gray-200 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-2xl"
+								className="block w-full rounded-md bg-slate-900 border border-slate-600 text-gray-200 placeholder-slate-500 mt-2 mb-4 py-2 pl-10 pr-3 text-sm focus:border-indigo-500 focus:text-gray-200 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-2xl"
 								placeholder="Search"
 							></input>
 						</div>
