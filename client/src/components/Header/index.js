a151aadb9f04dff2c88f21eec2fb6d6994da6c1b
Demi Hayashi <demi.h@me.com>
2022-11-14 20:37:37 -0800

    implement auth in nav menu

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -37,28 +37,31 @@ const Header = (props) => {
 							{/* NAVBAR AT 768PX & HIGHER */}
 							<div className="uppercase tracking-widest">
 								<div className="hidden md:flex bg-[#02030ac8] px-7 py-2 rounded-md font-semibold">
-									{/* AUTH NOT REQUIRED */}
-									<a href="/signup" className="mr-6">
-										Sign Up
-									</a>
-									<a href="/login" className="mx-6">
-										Log In
-									</a>
-									{/* END AUTH NOT REQUIRED */}
-									{/* AUTH REQUIRED */}
-									<a href="/search" className="mx-6">
-										Search
-									</a>
-									<a className="mx-6" href="/profile">
-										Profile
-									</a>
-									<button
-										onClick={logout}
-										className="bg-transparent hover:bg-transparent ml-6 text-[#6bfbce] hover:text-[#7286ff]"
-									>
-										Logout
-									</button>
-									{/* END AUTH REQUIRED */}
+									{/* {Auth.loggedIn() ? (
+										<> */}
+											<Link to="/search" className="mr-6">
+												Search
+											</Link>
+											<Link to="/profile" className="mx-6">
+												Profile
+											</Link>
+											<button
+												onClick={logout}
+												className="bg-transparent hover:bg-transparent ml-6 text-[#6bfbce] hover:text-[#7286ff]"
+											>
+												Logout
+											</button>
+										{/* </>
+										) : (
+										<> */}
+											<Link to="/signup" className="mr-6">
+												Sign Up
+											</Link>
+											<Link to="/login" className="ml-6">
+												Log In
+											</Link>
+										{/* </>
+									)} */}
 								</div>
 							</div>
 
