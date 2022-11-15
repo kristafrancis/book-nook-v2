52eb6a88e377c8361fb263fd13821e454efae178
Sam Karp <sfcypret@gmail.com>
2022-11-14 21:21:33 -0500

    removed console logs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      console.log("trying to workk")
+      
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).select(
           "-__v -password"
@@ -117,7 +117,7 @@ const resolvers = {
     saveBook: async (parent, args, context) => {
       console.log("saveBook")
       if (context.user) {
-        console.log(context.user, "not working")
+        console.log(context.user)
         const updateUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $push: { savedBooks: args.input } },
