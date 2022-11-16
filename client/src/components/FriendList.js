96e5055bc1b935f1d36b06a29d97d80260beb7b7
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-11-16 15:24:19 -0500

    comment box and friends list front end changes

--- client/src/components/FriendList.js
+++ client/src/components/FriendList.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const FriendList = ({ friendCount, username, friends }) => {
-    if (!friends || !friends.length) {
-        return <p class="friendmessage">{username}, make some friends! </p>;
-    }
+  if (!friends || !friends.length) {
+    return <p class="friendmessage">{username}, make some friends! </p>;
+  }
 
-    return (
-        <div>
-            <h5>
-                {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
-            </h5>
-            {friends.map(friend => (
-                <button className="btn w-100 display-block mb-2" key={friend._id}>
-                    <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h5>
+        {username}'s {friendCount} {friendCount === 1 ? "friend" : "friends"}
+      </h5>
+      {friends.map((friend) => (
+        <button
+          className="cursor-pointer bg-slate-800 inline-flex items-center justify-center rounded-md border border-indigo-200 px-4 py-2 text-sm font-medium text-indigo-200 shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+          key={friend._id}
+        >
+          <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default FriendList;
