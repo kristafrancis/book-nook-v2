688b509f4b31977ff4d7c980d0d0c6afb1ea034f
Demi Hayashi <demi.h@me.com>
2022-12-04 20:50:19 -0500

    redeploy to heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection')
+const db = require('./config/connection');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -31,7 +31,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     server.applyMiddleware({ app });
 
     if (process.env.NODE.ENV === 'production') {
-      app.use(express.static(path.join(_dirname, '../client/build')));
+      app.use(express.static(path.join(_dirname, '../client/build/index.html')));
     }
 
     db.once('open', () => {
